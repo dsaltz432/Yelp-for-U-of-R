c5905b8124bf33a7cc0f0fa540554606faa7a9e1
Daniel Saltz <dsaltz@u.rochester.edu>
2015-11-30 15:52:57 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,14 @@
+/*
+
+Used this for getting the current time in javascript
+http://stackoverflow.com/questions/12409299/how-to-get-current-formatted-date-dd-mm-yyyy-in-javascript-and-append-it-to-an-i
+
+
+*/
+
+
+
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -16,13 +27,13 @@ db.serialize(function() {
 		"(username TEXT NOT NULL,password TEXT NOT NULL,PRIMARY KEY (username))");
 
 	db.run("CREATE TABLE if not exists places_table "+
-		"(username TEXT NOT NULL,place TEXT NOT NULL, CONSTRAINT place_primary PRIMARY KEY (username,place))");
+		"(username TEXT NOT NULL,place TEXT NOT NULL,comment TEXT NOT NULL,rating TEXT NOT NULL,"+
+			"cost TEXT NOT NULL,time TEXT NOT NULL)");
 
 	// Reading user login attempts
 	app.get('/users/*', function (req, res) {
 		var myUsername = req.query.username.trim();
 		var myPassword = req.query.password.trim();
-		var resultString = "";
 
 		if (!myUsername && !myPassword){
 			res.send("Enter the required fieldsN");
@@ -51,7 +62,6 @@ db.serialize(function() {
 		var postBody = req.body;
 		var myUsername = postBody.username.trim();
 		var myPassword = postBody.password.trim();
-		var resultString = "";
 
 		if (!myUsername && !myPassword){
 			res.send("Enter the required fieldsN");
@@ -63,8 +73,7 @@ db.serialize(function() {
 			// making sure username and passwords are valid
 			if (myPassword.length < 7){
 				res.send("Your password must be longer than 7 characters");
-			} else {
-				
+			} else {	
 				db.run("INSERT INTO users_table VALUES (?,?)",[myUsername,myPassword], function(err, row) {
 					if (err != null){
 						res.send("Failed to create new accountN");
@@ -81,14 +90,18 @@ db.serialize(function() {
 	app.post('/places/', function (req, res) {
 		var postBody = req.body;
 		var place = postBody.place.trim();
-		var username =  postBody.username.trim();
+		var username = postBody.username.trim();
+		var comment = postBody.comment.trim();
+		var rating = postBody.rating.trim();
+		var cost = postBody.cost.trim();
+		var time = getDay();
 
 		if (!place){
 			res.send("Enter a place");
 		}
 		else {
-			console.log("place");
-			db.run("INSERT INTO places_table VALUES (?,?)",[username, place], function(err, row) {
+			db.run("INSERT INTO places_table VALUES (?,?,?,?,?,?)",
+			[username,place,comment,rating,cost,time], function(err, row) {
 				if (err != null){
 					res.send("Failed to add place");
 				} else {
@@ -97,6 +110,25 @@ db.serialize(function() {
 			});	
 		}
 	});
+
+function getDay(){
+	var today = new Date();
+	// var today = today.substring(4,today.length - 6);
+
+	// // var time = today.getTime();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+
+    var yyyy = today.getFullYear();
+    if(dd<10){
+        dd='0'+dd;
+    } 
+    if(mm<10){
+        mm='0'+mm;
+    } 
+    var today = mm+'/'+dd+'/'+yyyy;
+    return today;
+}
 
 // starting web server
 var server = app.listen(3000, function () {
