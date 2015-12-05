a3dcaa49f2f748344ce7436b7014769df1d70eaf
Daniel Saltz <dsaltz@u.rochester.edu>
2015-12-04 22:24:07 -0500

    updating repository to its current state

--- clearDatabase.js
+++
@@ -1,9 +0,0 @@
-// printing all elements from database.db
-var sqlite3 = require("sqlite3").verbose();
-var db = new sqlite3.Database("users.db");
-
-db.serialize(function() {
-	db.run("DELETE FROM users_table");
-});
-
-db.close();
